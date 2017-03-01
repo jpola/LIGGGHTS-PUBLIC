be168de171a749df9cb1cb22cd97afc467e674c7
Jakub Pola <jakub.pola@gmail.com>
2017-03-01 12:13:26 +0100

    LBM fixes

--- src/version_liggghts.h
+++ src/version_liggghts.h
@@ -1,1 +1,1 @@
-#define LIGGGHTS_VERSION "LIGGGHTS-PUBLIC 3.4.1, compiled 2017-01-31-09:43:46 by jpola, git commit 3ab54980cb42a6614b12d5bc27a078e3dfd8b4b7"
+#define LIGGGHTS_VERSION "LIGGGHTS-PUBLIC 3.4.1, compiled 2017-02-28-11:47:45 by jpola, git commit c8ccdec2754933db59657b3828115986b37f9a46"
